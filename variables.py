682099325c787fb1981cdbf4ecee528b51e896a3
toem1208 <101071945+toem1208@users.noreply.github.com>
2022-03-21 12:33:47 +1300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,12 @@
+# code to learn about variables
+
+# variables can be a string 
+greeting = "hello world"
+
+# they can aslo be numbers 
+fav_num = 7
+
+# Below is a constant (the name is in UPPERCASE)
+PI = 3.14
+
+num_2 = 2
